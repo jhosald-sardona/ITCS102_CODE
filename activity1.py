c4b65f3a8d32a85a16bde5a72825fa261cce3697
jhosald-sardona <jhosalds@gmail.com>
2026-09-03 20:51:53 +0800

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,21 @@
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+Jhosald Machain D. Sardona
+
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
+Bachelor of Science Information Technology
