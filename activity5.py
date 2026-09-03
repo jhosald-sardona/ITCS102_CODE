0bafb99c95f536f9e87de6443e76ce9b7c36d892
jhosald-sardona <jhosalds@gmail.com>
2026-09-03 22:18:15 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,34 @@
+#Operators
+#Activity5 
+
+#Variables 
+a = 10 
+b = 5 
+
+#Addition 
+sum = a + b 
+print("The sum of", a, "and", b, "is", sum) 
+
+#Substraction 
+difference = a - b 
+print("The difference of", a, "and", b, "is", difference) 
+
+#Multiplication 
+product = a * b 
+print("The product of", a, "and", b, "is", product) 
+
+#Division 
+quotient = a / b 
+print("The quotient of", a, "and", b, "is", quotient) 
+
+#Floor Division 
+floordivision = a // b 
+print("The floordivison of", a, "and", b, "is", floordivision)
+
+#Exponent 
+exponent = a ** b 
+print("10 to the 5th power is", exponent)  
+
+#Modulus 
+modulus = a % b 
+print("Modulus of", a, "and", b, "is", modulus)
