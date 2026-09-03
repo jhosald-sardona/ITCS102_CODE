3980c4073357fb9004b5c2c2727f5b4e1166f080
jhosald-sardona <jhosalds@gmail.com>
2026-09-03 11:20:57 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,6 @@
+phrase = "\t\t\t\t\t\t*\n \t\t\t\t\t* \t\t* \n \t\t\t\t* \n \t\t\t* \t\t\t\t\t\t* \n \t\t* \t\t\t\t\t\t\t\t* \n \t* \t\t\tmy name is \"JHOSALD SARDONA\"\t\t\t* \n \t\t* \t\t\t\t\t\t\t\t* \n \t\t\t* \t\t\t\t\t\t* \n \t\t\t\t* \t\t\t\t* \n \t\t\t\t\t* \t\t* \n \t\t\t\t\t\t\t*"
+
+
+
+print (phrase)
+
