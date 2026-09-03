db4aa883bdd03eccad343e1435f5a04c5d399fdc
jhosald-sardona <jhosalds@gmail.com>
2026-09-03 21:58:34 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,27 @@
+#Activity
+
+#integer 
+age = 18
+number = 9947057236
+emergency_contact = 9563119976
+
+#float 
+price = 30.45
+temperature = 31.30
+measurement = 22.33
+
+#String 
+name = "Jhosald Sardona,"
+birthday = "April 11, 2008"
+country = "Philippines" 
+
+#boolean 
+isFemale = False 
+isMale = True
+isSingle = True
+
+#Print 
+print ("Hello my name is ", name, "I live in the ", country) 
+print ("and my age right now is ",age, "years old") 
+print ("I was born on ", birthday) 
+print ("The Bananaque costs ",price, "Pesos")
